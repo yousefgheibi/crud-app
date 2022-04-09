09889a82477e6d4714edcc905fa786692da0ff16
yousef <gheibiyousef2000@gmail.com>
2022-04-09 12:05:06 +0430

    implement get single user api

--- backend/index.js
+++ backend/index.js
@@ -13,17 +13,17 @@ app.use(bodyParser.json());
 // database connerction 
 
 const db = mysql.createConnection({
-   host: 'localhost',
-   user: 'root',
-   password: '',
-   database: 'simpledb',
-   port:3306
-}); 
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'simpledb',
+  port: 3306
+});
 
 // check database connection
 
-db.connect(err=>{
-  if(err){
+db.connect(err => {
+  if (err) {
     console.log(err);
   }
   console.log('database connect ....');
@@ -32,17 +32,17 @@ db.connect(err=>{
 
 
 // get all data 
-app.get('/user',(req,res)=>{
+app.get('/user', (req, res) => {
   let qry = 'select * from user';
 
-  db.query(qry,(err,result)=>{
-    if(err){
+  db.query(qry, (err, result) => {
+    if (err) {
       console.log(err);
     }
 
-    if(result.length>0){
+    if (result.length > 0) {
       res.send({
-        message: 'all user data  .',
+        message: 'all user data.',
         data: result
       })
     }
@@ -50,9 +50,35 @@ app.get('/user',(req,res)=>{
 })
 
 
+// get single user
+app.get('/user/:id', (req, res) => {
+  let gId = req.params.id;
+
+  let qry = `select * from user where id=${gId}`;
+
+  db.query(qry, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    if (result.length > 0) {
+      res.send({
+        message: 'get single data.',
+        data: result
+      })
+
+    }
+    else {
+      res.send({
+        message: 'data not found.'
+      })
+    }
+  })
+
+})
+
 app.listen(3000, () => {
   console.log("server is running");
 });
-  
+
 
 
