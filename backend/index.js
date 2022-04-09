b9388b5bec9c9d83b047fa0ebb57f278bc7ef35f
yousef <gheibiyousef2000@gmail.com>
2022-04-09 11:16:21 +0430

    connection database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 // const cros = require("cros");
+const mysql = require("mysql2");
+
 
 const app = express();
 
@@ -8,9 +10,28 @@ const app = express();
 app.use(bodyParser.json());
 
 
+// database connerction 
 
+const db = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   database: 'simpledb',
+   port:3306
+}); 
+
+// check database connection
+
+db.connect(err=>{
+  if(err){
+    console.log(err);
+  }
+  console.log('database connect ....');
+});
 
 app.listen(3000, () => {
   console.log("server is running");
 });
   
+
+
